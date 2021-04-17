6e3642ed1ec8c686818674ea1239c8d2c07f7f5b
tatiantunes <tativive@hotmail.com>
2021-04-17 16:21:14 -0300

    feat(deploy)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'YOUR_API_KEY',
-  databaseURL: 'YOUR_DATABASE_URL'
+  apiKey: 'AAAAzeRkJqQ:APA91bEpJgICinKGlW8xtM82IR_tqrffCaAEVYGS01g3eRipcF5h7FQKBRMRKgerLpWxHRhSYBrT1yklVkSFng6Szx3PlN7iej2EYsVoe6CSPHFHhRxDL1pB22xk92BKk6I_67rbNiT8',
+  databaseURL: 'https://find-your-help-default-rtdb.firebaseio.com'
 };
 
 @Component({
